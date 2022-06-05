61a3264c64e728b29db9fac7ac71f417a29b1402
BritKig <97478354+britkig@users.noreply.github.com>
2022-06-05 10:40:40 +0100

    Update system.php

--- share/php/harmony/cli/system.php
+++ share/php/harmony/cli/system.php
@@ -1,9 +1,9 @@
 <?php	//	Harmony Framework, CLI Components
 
 //	Load common modules
-foreach(glob(implode(DIRECTORY_SEPARATOR,[dirname(__DIR__),'common','system.php'])) as $_)
-		require_once $_;
+foreach(glob(implode(DIRECTORY_SEPARATOR,[dirname(__DIR__),'system.php'])) as $_)
+	require_once $_;
 
 //	Load modules
 foreach(glob(implode(DIRECTORY_SEPARATOR,[__DIR__,'*.php'])) as $_)
-		require_once $_;
+	require_once $_;
