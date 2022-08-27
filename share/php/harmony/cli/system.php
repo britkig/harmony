ebab0c366d0742c92b2663b7e5a06634a1b40f6f
BritKig <97478354+britkig@users.noreply.github.com>
2022-08-27 10:53:35 +0100

    Delete share/php/harmony/cli directory

--- share/php/harmony/cli/system.php
+++
@@ -1,9 +0,0 @@
-<?php	//	Harmony Framework, CLI Components Loader
-
-//	Load common modules
-foreach(glob(implode(DIRECTORY_SEPARATOR,[dirname(__DIR__),'system.php'])) as $_)
-	require_once $_;
-
-//	Load modules
-foreach(glob(implode(DIRECTORY_SEPARATOR,[__DIR__,'*.php'])) as $_)
-	require_once $_;
