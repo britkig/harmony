efb3cb2d3b4829a120146f5d0776c8e243a76c6e
BritKig <97478354+britkig@users.noreply.github.com>
2022-08-27 11:10:45 +0100

    Update discord.embed.php

--- modules/discord/discord.embed.php
+++ modules/discord/discord.embed.php
@@ -13,54 +13,54 @@ final class Embed{
 	private $timestamp=null;
 
 	private $color=null;
-	static function color(self&$_,null|int$a=null):void{
+	static function color(self &$_,null|int$a=null):void{
 		$_->color=$a;
 	}
 
 	private $title=null;
-	static function title(self&$_,null|string$a=null):void{
+	static function title(self &$_, null|string $a=null):void{
 		$_->title=$a;
 	}
 
 	private $description=null;
-	static function Description(self&$_,null|string$a=null):void{
+	static function Description(self &$_,null|string $a=null):void{
 		$_->description=$a;
 	}
 
 	private $footer=null;
-	static function Footer(self&$_,null|string$t=null,null|string$i=null):void{
+	static function Footer(self &$_, null|string $t=null, null|string $i=null):void{
 		if(!$t){
 			$_->footer=null;
 			return;
 		}
 		$_->footer=['text'=>$t];	
 		if($i)
-      $_->footer['icon_url']=$i;
+    		$_->footer['icon_url']=$i;
 	}
 
 	private $image=null;
-	static function Image(self&$_,null|string$a=null):void{
+	static function Image(self &$_,null|string $a=null):void{
 		$_->image=($a?['url'=>$a]:null);
 	}
 
 	private $thumbnail=null;
-	static function Thumbnail(self&$_,null|string$a=null):void{
+	static function Thumbnail(self &$_,null|string $a=null):void{
 		$_->thumbnail=($a?['url'=>$a]:null);
 	}
 
 	private $author=null;
-	static function Author(self&$_,null|string$a=null):void{
+	static function Author(self &$_, null|string $a=null):void{
 		$_->author=($a?['url'=>$a]:null);
 	}
 
-	static function Build(self$_):array{
+	static function Build(self $_):array{
 		$_=\get_object_vars($_);
 		foreach(\array_keys($_)as$a)
       if(is_null($_[$a]))
         unset($_[$a]);
 	return $_;}
 
-	static function Create(&$_,null|bool$t=true):void{
+	static function Create(&$_, null|bool $t=true):void{
 		$_=new self();
 		if($t)$_->timestamp=\date(DATE_ISO8601);
 	}
