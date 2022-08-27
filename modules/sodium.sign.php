87d1c570d7213a8d7a7fbbf087168c40b18c350c
BritKig <97478354+britkig@users.noreply.github.com>
2022-08-27 11:45:47 +0100

    Update and rename sodium.php to sodium.sign.php

--- modules/sodium.sign.php
+++ modules/sodium.sign.php
@@ -1,5 +1,5 @@
 <?php namespace Sodium;
 
-function VerifyDetached(string $s,string $m, string $pk):bool{
+function SignVerifyDetached(string $s,string $m, string $pk):bool{
 	return \sodium_crypto_sign_verify_detached($s,$m,$pk);
 }
