47d0f1e058fac9183c6b9e3193aa4e043cc26cb0
BritKig <97478354+britkig@users.noreply.github.com>
2022-08-27 11:53:31 +0100

    Update sodium.php

--- modules/sodium/sodium.php
+++ modules/sodium/sodium.php
@@ -1,5 +1,9 @@
 <?php namespace Sodium;
 
+function BinHex(&$a, string $b):void{
+  $a=\sodium_bin2hex($b);
+}
+
 function HexBin(&$a, string $b, string $c=''):void{
   $a=\sodium_hex2bin($b,$c);
 }
