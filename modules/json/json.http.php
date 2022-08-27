627ddeaba3189d7e7df2f189ebe619cf9357f20c
BritKig <97478354+britkig@users.noreply.github.com>
2022-08-27 12:18:18 +0100

    Update json.http.php

--- modules/json/json.http.php
+++ modules/json/json.http.php
@@ -1,7 +1,7 @@
 <?php namespace JSON;
-  const HTTP_MIME='application/json';
+	const HTTP_MIME='application/json';
 
 function EncodeHTTP(array &$a):bool{
-  if(!Encode($b,$a)) return false;
-  if(!\HTTP\Response($b,HTTP_MIME)) return false;
+	if(!Encode($b,$a)) return false;
+	if(!\HTTP\Response($b,HTTP_MIME)) return false;
 return true;}
