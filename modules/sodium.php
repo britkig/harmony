3b542c523e18c3e9ab2552248432bd69cb8eabfa
BritKig <97478354+britkig@users.noreply.github.com>
2022-08-27 11:44:18 +0100

    Update sodium.php

--- modules/sodium.php
+++ modules/sodium.php
@@ -1,5 +1,5 @@
 <?php namespace Sodium;
 
-function VerifyDetached(string $a,string $b, string $c):bool{
-	return \sodium_crypto_sign_verify_detached($a,$b,$c);
+function VerifyDetached(string $s,string $m, string $pk):bool{
+	return \sodium_crypto_sign_verify_detached($s,$m,$pk);
 }
