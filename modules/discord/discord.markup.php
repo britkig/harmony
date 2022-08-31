34a324a3b01da4fe3342307e9864d96f5df9a87e
BritKig <97478354+britkig@users.noreply.github.com>
2022-08-31 18:33:23 +0100

    Create discord.markup.php

---
+++ modules/discord/discord.markup.php
@@ -0,0 +1,35 @@
+<?php namespace Discord\Markup;
+
+const Code='`%s`';
+function Code(string$a,string$b=null):string{
+	return \sprintf(Code,$a,$b);
+}
+
+const CodeBlock='```%s
+%s```';
+function CodeBlock(string$a,string$b=null):string{
+	return \sprintf(CodeBlock,$b,$a);
+}
+
+const MentionChannel='`<#%s>`';
+function MentionChannel(int$a):string{
+	return \sprintf(MentionChannel,$a);
+}
+
+const MentionUser='`<@%s>`';
+function MentionUser(int$a):string{
+	return \sprintf(MentionUser,$a);
+}
+
+const TIMEDATE='`<t:%s%s>`';
+const TIMEDATE_DATELONG=':D';
+const TIMEDATE_DATESHORT=':d';
+const TIMEDATE_FULLLONG=':F';
+const TIMEDATE_FULLSHORT=':f';
+const TIMEDATE_RELATIVE=':R';
+const TIMEDATE_TIMELONG=':T';
+const TIMEDATE_TIMESHORT=':t';
+
+function TimeDate(int $a, string $b=TIMEDATE_FULLLONG):string{
+	return \sprintf(TIMEDATE,$a,$b);
+}
