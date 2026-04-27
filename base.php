361e1a92c49393b922ea2f8c996f566f02be2106
BritKig <97478354+britkig@users.noreply.github.com>
2026-04-27 11:47:08 +0100

    Update base.php

--- base.php
+++ base.php
@@ -3,18 +3,17 @@ abstract class Base{
 	static final function HasAttribute(\ReflectionObject $r, string $p, string $a):bool{
 		return (bool)$r->getProperty($p)->getAttributes($a::class);
 	}
-	final function MapV(array $m, ?array $d):void{
-		if($m)
-			if($d){
-				foreach($d as $a=>$b){
-					
-				}
-			}else{
-				foreach($m as $a=>$b)
-					if(\is_string($a))
-						if(\property_exists($this,$a))
-							$this->$a=$b;
+	final function MapV(array $d, ?array $m):void{
+		if($m){
+			foreach($m as $a=>$b){
+				
 			}
+		}else{
+			foreach($d as $a=>$b)
+				if(\is_string($a))
+					if(\property_exists($this,$a))
+						$this->$a=$b;
+		}
 	}
 	final Function Map(array $m, ... $d):void{
 		MapV($m, $d);
@@ -23,6 +22,7 @@ abstract class Base{
 	protected function Initialise():void{}
 	final function __construct(array $d=[], ?array $m){
 		$this->initialize();
-		$this->MapV($d, $m);
+		if($d)
+			$this->MapV($d, $m);
 	}
 }
