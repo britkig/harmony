6702f49364ead980f1497378583682407e1776d4
BritKig <97478354+britkig@users.noreply.github.com>
2026-04-27 09:35:28 +0100

    Update base.php

--- base.php
+++ base.php
@@ -1,20 +1,13 @@
 <?php
 abstract class Base{
-
 	private readonly \ReflectionObject $_meta;
-
 	final function HasAttribute(string $p,string $a):bool{
 		return (bool)$this->_meta->getProperty($p)->getAttributes($a::class);
 	}
-
 	#[\Override]
-	protected function initialise():void{
-		//	No-op: overridable for initialising properties on-create.
-	}
-
+	protected function Initialise():void{}
 	final function __construct(?array $data=[],?array $map=[]){
 		$this->$_meta=\ReflectionObject($this);
 		$this->initialize();
 	}
-
 }
