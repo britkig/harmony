0f50815d74340bc69c81e3cac08b6dd8d8e14154
BritKig <97478354+britkig@users.noreply.github.com>
2026-04-27 10:09:02 +0100

    Update encode.php

--- .function/json/encode.php
+++ .function/json/encode.php
@@ -1,3 +1,5 @@
 <?php namespace JSON;
-function Encode(&$_, array $a, int $f=\JSON_PRESERVE_ZERO_FRACTION+\JSON_THROW_ON_ERROR+\JSON_NUMERIC_CHECK+\JSON_UNESCAPED_SLASHES):void{
-	$_=\json_encode($a,$f);}
+function Encode(&$_, array $a):bool{
+	if(($a=@\json_encode($a,\JSON_PRESERVE_ZERO_FRACTION+\JSON_THROW_ON_ERROR+\JSON_NUMERIC_CHECK+\JSON_UNESCAPED_SLASHES))===false) return false;
+	$_=$a;
+	return true;}
