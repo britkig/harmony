69d0cd05671b2e75ed058827e268b2f8a19d71b9
BritKig <97478354+britkig@users.noreply.github.com>
2026-04-27 09:37:18 +0100

    Update base.php

--- base.php
+++ base.php
@@ -4,10 +4,25 @@ abstract class Base{
 	final function HasAttribute(string $p,string $a):bool{
 		return (bool)$this->_meta->getProperty($p)->getAttributes($a::class);
 	}
+	final function Map(array $data=[],array $map=[]){
+		if($data)
+			if($map){
+				foreach($data as $a=>$b){
+					
+				}
+			}else{
+				foreach($data as $a=>$b)
+					if(\is_string($a))
+						if(\property_exists($this,$a))
+							$this->$a=$b;
+			}
+	}
 	#[\Override]
 	protected function Initialise():void{}
 	final function __construct(?array $data=[],?array $map=[]){
 		$this->$_meta=\ReflectionObject($this);
 		$this->initialize();
+		if($data)
+			$this->Map($data,$map);
 	}
 }
