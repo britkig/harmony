39ba6a3e43694c7687a303129db6465f5465dc45
BritKig <97478354+britkig@users.noreply.github.com>
2026-04-27 10:09:45 +0100

    Update decode.php

--- .function/json/decode.php
+++ .function/json/decode.php
@@ -1,3 +1,5 @@
 <?php namespace JSON;
-function Decode(&$_, string $s):void{
-	$_=\json_decode($s, true, \JSON_THROW_ON_ERROR);}
+function Decode(&$_, string $s):bool{
+	if(($s=@\json_decode($s,true,\JSON_THROW_ON_ERROR))===false) return false;
+	$_=$s;
+	return true;}
