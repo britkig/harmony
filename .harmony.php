d1d0e2c416204e2714121fc4ffbb25f744c9f58d
BritKig <97478354+britkig@users.noreply.github.com>
2026-04-28 19:03:16 +0100

    Update .harmony.php

--- .harmony.php
+++ .harmony.php
@@ -4,10 +4,8 @@ const Root=__DIR__;
 const DirSeparator=\DIRECTORY_SEPARATOR;
 
 \set_error_handler(function(int $n, string $s,string $f, int $l){
-	if(!(\error_reporting() & $n)) 
-		return;
-	if($n==\E_DEPRECATED || $n==\E_USER_DEPRECATED)
-		return;
+	if(!(\error_reporting() & $n)) return;
+	if($n==\E_DEPRECATED || $n==\E_USER_DEPRECATED) return;
 	throw new \ErrorException($s, $n, $n, $f, $l);});
 
 function ReturnUnset(&$_){
@@ -15,8 +13,13 @@ function ReturnUnset(&$_){
 	return $a;}
 
 function SecureInclude($_){
-	return @include_once ReturnUnset($_);}
+	return @require_once ReturnUnset($_);}
 
-\spl_autoload_register(callback:function(string$_):void{
+\spl_autoload_register(callback:function(string $_):void{
 	if(\is_file($_=\implode(DirSeparator,[Root,\strtolower($_).'.php'])))
 		SecureInclude($_);});
+
+foreach(\iterator_to_array(new \RegexIterator(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator(\implode(DirSeparator,[Root,'.function']))),'/\.php$/')) as $_)
+	SecureInclude($_);
+
+unset($_);
